4013f7f0b5f95139caa18c57cbd83601c3f374e0
Sarah Sanchez Leon <SarahSanchezLeon@hotmail.com>
2018-04-18 10:23:55 +0100

    Fixed problem

--- TFT/Assets/MyAssets/Script/Managers/MainStates/GMS_ScanningRoom.cs
+++ TFT/Assets/MyAssets/Script/Managers/MainStates/GMS_ScanningRoom.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 #if UNITY_EDITOR
-using Input = InstantPreviewInput;
+using Input = GoogleARCore.InstantPreviewInput;
 #endif
 public class GMS_ScanningRoom : GMS_ControllerState
 {
