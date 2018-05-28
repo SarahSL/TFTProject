cad224fb51cdce25245e0446725c8c8c104f2d0d
Sarah Sanchez Leon <SarahSanchezLeon@hotmail.com>
2018-05-28 13:52:53 +0100

    2017.4.0 version of unity

--- TFT/Assets/MyAssets/Script/Managers/MainStates/GMS_Video.cs
+++ TFT/Assets/MyAssets/Script/Managers/MainStates/GMS_Video.cs
@@ -1,18 +1,24 @@
 ﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GMS_Video /*: GMS_ControllerState*/
+public class GMS_Video : GMS_ControllerState
 {
+    public override void Enter()
+    {
+        throw new NotImplementedException();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void Exit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
+    
 }
