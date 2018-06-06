78dcc62fe6d1e00db5109e74086d1a3f25ff86e8
Sarah Sanchez Leon <SarahSanchezLeon@hotmail.com>
2018-06-06 11:41:49 +0100

    wip

--- TFT/Assets/MyAssets/Script/Truck/TSC_Waiting.cs
+++ TFT/Assets/MyAssets/Script/Truck/TSC_Waiting.cs
@@ -9,7 +9,7 @@ public class TSC_Waiting : TSC_TruckControllerState
     public override void Enter()
     {
         Debug.Log("TRUCK STATE WAITING");
-        m_target.SM_GoToAngry();
+        
     }
 
     public override void Exit()
